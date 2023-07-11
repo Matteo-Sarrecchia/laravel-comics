2742998bd22a84e0100b60c9cfe6daf7f6902cbe
Matteo Sarrecchia <morrea@hotmail.it>
2023-07-11 17:09:58 +0200

    first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,5 @@
+<div class="container-fluid bg-black text-center">
+    
+    <h1>HEADER</h1>
+
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.main-layout')
+
+@section('pageName')
+
+    home
+
+@endsection
+
+@section('content')
+
+    <main class="bg-dark py-5">
+       <div class="container"> 
+            <div> 
+                
+            </div>
+       </div>
+    </main>
+
+
+
+@endsection
