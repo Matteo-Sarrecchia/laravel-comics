aaa2efbc7bc583a7428b2e40fa8942e12f7d6986
Matteo Sarrecchia <morrea@hotmail.it>
2023-07-11 18:51:32 +0200

    base header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,15 @@
-<div class="container-fluid bg-black text-center">
-    
-    <h1>HEADER</h1>
+<div class="container-fluid text-center py-3">
+    <div class="row">
+        <div class="d-flex justify-content-around">
+            <a href="" style="text-decoration: none;color: black;font-size: 20px;">LOGO</a>
+            <a href="" style="text-decoration: none;color: black;font-size: 20px;">CHARACTERS</a>
+            <a href="" style="text-decoration: none;color: black;font-size: 20px;">COMICS</a>
+            <a href="" style="text-decoration: none;color: black;font-size: 20px;">SHARE</a>
+            <a href="" style="text-decoration: none;color: black;font-size: 20px;">ALL</a>
+            <a href="" style="text-decoration: none;color: black;font-size: 20px;">CONTACT</a>
+            <input type="text" placeholder="SEARCH">
+        </div>
+            
+    </div>
 
 </div>
